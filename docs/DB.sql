135a02a67cdaa54c70863ac5de676cfce89ee91d
Márton Spiteller <spitellermarton@gmail.com>
2026-02-01 13:06:09 +0100

    update: necessary sql commands

--- docs/DB.sql
+++ docs/DB.sql
@@ -13,7 +13,9 @@ CREATE TABLE IF NOT EXISTS test_cases (
 
 INSERT INTO projects (name) VALUES (?);
 SELECT * FROM projects;
-DELETE FROM projects WHERE id = ?; -- TODO: also delete correspondind cases
+DELETE FROM test_cases WHERE project_id = ?; -- delete test cases before project
+DELETE FROM projects WHERE id = ?; -- after test cases deleted, delete project
+
 
 INSERT INTO test_cases (project_id, case_number/* ??? */, case_settings) VALUES (?, ?, ?);
 SELECT * FROM test_cases WHERE project_id = ?; -- get all cases
@@ -21,6 +23,3 @@ SELECT * FROM test_cases WHERE id = ?; -- get one case
 UPDATE test_cases WHERE id = ? SET case_settings = ?; -- update case settings
 UPDATE test_cases WHERE id = ? SET case_result = ?; -- update case result
 DELETE FROM test_cases WHERE id = ?; -- dedlete case
-
-
-
